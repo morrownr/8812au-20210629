74f5d89eb75dcf7419a8519b56130100e8513589
anush elangovan <anush@nod-labs.com>
2022-07-24 23:38:39 -0700

    Add a few more tested USB devices
    TP-Link Archer T9UH v1
    NetGear, Inc. A7000
    AC1750
    ASUSTek Computer, Inc. 802.11ac
    NetGear, Inc. A7000

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -235,7 +235,12 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 	{USB_DEVICE(0x0b05, 0x1817), .driver_info = RTL8814A}, /* ASUS - ASUSTeK */
 	{USB_DEVICE(0x056E, 0x400B), .driver_info = RTL8814A}, /* ELECOM - ELECOM */
 	{USB_DEVICE(0x056E, 0x400D), .driver_info = RTL8814A}, /* ELECOM - ELECOM */
+	{USB_DEVICE(0x7392, 0xA833), .driver_info = RTL8814A}, /* Edimax AC1750 */
 	{USB_DEVICE(0x7392, 0xA834), .driver_info = RTL8814A}, /* Edimax - Edimax */
+	{USB_DEVICE(0x2357, 0x0106), .driver_info = RTL8814A}, /* TP-Link Archer T9UH v1 */
+	{USB_DEVICE(0x0846, 0x9054), .driver_info = RTL8814A}, /* NetGear, Inc. A7000 */
+	{USB_DEVICE(0x0b05, 0x1852), .driver_info = RTL8814A}, /* ASUSTek Computer, Inc. 802.11ac */
+	{USB_DEVICE(0x0846, 0x9054), .driver_info = RTL8814A}, /* NetGear, Inc. A7000 */
 #endif /* CONFIG_RTL8814A */
 
 #ifdef CONFIG_RTL8188F
